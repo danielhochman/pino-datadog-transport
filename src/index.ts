89d9ef068ce56fff53cd95f174109823251bb698
Daniel Hochman <danielhochman@users.noreply.github.com>
2023-11-01 14:44:52 -0500

    dont use mixed statements

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import build from 'pino-abstract-transport'
 import { client, v2 } from '@datadog/datadog-api-client'
 import { DDTransportOptions, processLogBuilder } from './process-logs'
 
-module.exports = function (options: DDTransportOptions) {
+export default function (options: DDTransportOptions) {
   const configuration = client.createConfiguration(options.ddClientConf)
   client.setServerVariables(configuration, options?.ddServerConf || {})
   const apiInstance = new v2.LogsApi(configuration)
